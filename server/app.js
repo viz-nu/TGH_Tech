050050521845ce801e9bf1d14b42949375797682
viz-nu/ <vishnu.teja101.vt@gmail.com>
2023-04-06 04:23:40 +0530

    build done

--- server/app.js
+++ server/app.js
@@ -4,6 +4,23 @@ import "./dbConnect.js";
 import cors from "cors";
 const app = express();
 app.use(cors())
+
+//...................deployement.......................
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
+//....................................................
+
 app.use(express.json());
 import userRouter from "./controllers/user/index.js";
 app.use("/api/user", userRouter);
